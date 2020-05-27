36cf7002b1380bac7ea404e5f8468997efc7298d
goeunleee <goeun3501@gmail.com>
2020-05-27 12:27:01 +0900

    se2d

--- ch03/src/ch03/operator/Operator.java
+++ ch03/src/ch03/operator/Operator.java
@@ -7,17 +7,25 @@ public class Operator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-        int x=10; int y = 1010;
+        int x=7; int y = 1010;
         x ++; 
         System.out.println("x is "+x+"  y is "+y);
         System.out.println(Integer.toBinaryString(y));
         Scanner in = new Scanner(System.in);
-        x = in.nextInt();
+       //x = in.nextInt();
         System.out.println(x);
+        double z = y/x;
+        System.out.println(z);
+        System.out.println(Double.isFinite(z));
+        System.out.println("jdk"+11);
+        System.out.println("총점을 입력하세요 :");
         
         
+        int sum = in.nextInt();
+        String  grade = sum >= 80 ? "합격": "불합격";
         
-        System.out.println(toBinaryString(y));
+       System.out.println(grade);
+        //System.out.println(toBinaryString(y));
 	}
 	public static String toBinaryString(int value) {
 		String str = Integer.toBinaryString(value);
