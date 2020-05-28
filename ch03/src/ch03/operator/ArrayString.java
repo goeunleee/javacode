d7c1ec371af75fdaad08f9572417057d75685995
goeunleee <goeun3501@gmail.com>
2020-05-28 15:34:15 +0900

    ㅋㅋㅋ

---
+++ ch03/src/ch03/operator/ArrayString.java
@@ -0,0 +1,12 @@
+package ch03.operator;
+
+public class ArrayString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    System.out.println("args ±Ê¿Ã "+args.length); 
+	    
+	    
+	}
+
+}
