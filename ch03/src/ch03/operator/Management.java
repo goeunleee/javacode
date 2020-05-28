1068bbab284feadd0a7c626a578ca72cf62c105f
goeunleee <goeun3501@gmail.com>
2020-05-28 14:34:02 +0900

    array

--- ch03/src/ch03/operator/Management.java
+++ ch03/src/ch03/operator/Management.java
@@ -1,9 +1,24 @@
 package ch03.operator;
 
+import java.util.Scanner;
+
 public class Management {
 
 	public static void main(String[] args) {
-    
+        int []score = new int[10];
+        Scanner in = new Scanner(System.in);
+        for(int i=0; i<10; i++)
+        	score[i]= in.nextInt();
+        
+        double sum =0;
+        for(int i=0; i<10; i++) {
+        	sum += score[i];
+        }
+        System.out.println("ÃÑÇÕÀº : "+sum);
+        
+       double avg = sum/10;
+        
+        System.out.println("Æò±ÕÀº : "+avg);
 	}
 
 }
