185b3efc5a73f53319a76fcd2fae9e3b74e78d63
goeunleee <goeun3501@gmail.com>
2020-05-28 17:22:58 +0900

    28일

--- ch03/src/ch03/operator/ArrayString.java
+++ ch03/src/ch03/operator/ArrayString.java
@@ -1,12 +1,57 @@
 package ch03.operator;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ArrayString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	    System.out.println("args 길이 "+args.length); 
-	    
-	    
+		int[][] arry = new int[3][3];
+		Scanner sc = new Scanner(System.in);
+		int menu = 0, sum = 0;
+		double avg = 0;
+
+		while (menu != 3) {
+			System.out.println("1. 학생 점수 입력 2. 결과 출력 3. 종료");
+			menu = sc.nextInt();
+
+			if (menu == 1) {
+
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						int score = sc.nextInt();
+						arry[i][j] = score;
+
+					}
+				}
+			}
+
+			if (menu == 2) {
+				for (int i = 0; i < 3; i++) {
+					System.out.print("학생 " + (i + 1) + " : ");
+
+					for (int j = 0; j < 3; j++) {
+
+						System.out.print(arry[i][j] + "점 ");
+						sum += arry[i][j];
+					}
+
+					avg = Double.parseDouble(String.format("%.2f", (double) sum / 3));
+					System.out.println(" 총점은 " + sum + " 평균은 " + avg);
+					sum = 0;
+
+				}
+				double kor = 0, eng = 0, math = 0;
+				for (int i = 0; i < 3; i++) {
+					kor += arry[i][0];
+					eng += arry[i][1];
+					math += arry[i][2];
+				}
+				System.out.println("국 : " + (kor / 3) + " 영 : " + (eng / 3) + " 수 : " + (math / 3));
+			}
+		}
+		System.out.println("종료합니다");
+
 	}
 
 }
