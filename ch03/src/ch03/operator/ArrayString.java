39001d7a943aea7bfab981932d3a143cc9f359b3
goeunleee <goeun3501@gmail.com>
2020-05-29 09:46:27 +0900

    5/29-1

--- ch03/src/ch03/operator/ArrayString.java
+++ ch03/src/ch03/operator/ArrayString.java
@@ -1,10 +1,10 @@
 package ch03.operator;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class ArrayString {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		int[][] arry = new int[3][3];
 		Scanner sc = new Scanner(System.in);
@@ -16,8 +16,9 @@ public class ArrayString {
 			menu = sc.nextInt();
 
 			if (menu == 1) {
-
+                  
 				for (int i = 0; i < 3; i++) {
+					System.out.println("학생 "+(i+1)+" 의 국영수 점수를 입력하세요. :");
 					for (int j = 0; j < 3; j++) {
 						int score = sc.nextInt();
 						arry[i][j] = score;
@@ -47,7 +48,7 @@ public class ArrayString {
 					eng += arry[i][1];
 					math += arry[i][2];
 				}
-				System.out.println("국 : " + (kor / 3) + " 영 : " + (eng / 3) + " 수 : " + (math / 3));
+				System.out.println("\n국어 평균 : " + (kor / 3) + " 영어 평균 : " + (eng / 3) + " 수학 평균 : " + (math / 3)+"\n");
 			}
 		}
 		System.out.println("종료합니다");
---
+++ ch05/src/ch05/Week.java
@@ -0,0 +1,15 @@
+package ch05;
+
+public enum Week {
+
+	
+		MONDAY,
+		TUESDAY,
+		WENDSESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY
+	
+
+}
