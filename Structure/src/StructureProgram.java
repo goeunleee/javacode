0cc06e1928c7f8b16c4e88690cf246c87fb116da
goeunleee <goeun3501@gmail.com>
2020-06-08 15:07:46 +0900

    0608-2

--- Structure/src/StructureProgram.java
+++ Structure/src/StructureProgram.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class StructureProgram {
 
 	static Scanner scan = new Scanner(System.in);
-	static int[] kors = new int[3];
-	static int total = 0;
-	static float avg;
+
+	static int[][] student = new int[3][3]; // 국영수, 학생3명
+	static int total[] = new int[3];
+	static float avg[] = new float[3];
 
 	public static void main(String[] args) {
 
@@ -25,12 +26,12 @@ public class StructureProgram {
 			switch (menu) {
 			case 1:
 
-				Grade.inputGrade(kors);
+				Grade.inputGrade(student);
 
 				break;
 			case 2:
 
-				print(kors);
+				print(student);
 
 				break;
 			case 3:
@@ -46,31 +47,44 @@ public class StructureProgram {
 		scan.close();
 	}
 
-	static void print(int[] kors) {
+	static void print(int[][] student) {
 
-		for (int i = 0; i < 3; i++)
-			total += kors[i];
+		for (int i = 0; i < 3; i++) {
+			total[0] += student[0][i];
+			total[1] += student[1][i];
+			total[2] += student[2][i];
+		}
 
-		avg = total / 3.0f;
+		avg[0] = total[0] / 3.0f;
+		avg[1] = total[1] / 3.0f;
+		avg[2] = total[2] / 3.0f;
 
 		System.out.println("┌───────────────────────────┐");
 		System.out.println("│           성적  출력      │");
 		System.out.println("└───────────────────────────┘");
 		System.out.println();
 
-		for (int i = 0; i < 3; i++)
-			System.out.printf("국어 %d : %3d\n", 3 - i, kors[i]);
-
-		System.out.printf("총점 : %3d\n", total);
-		System.out.printf("평균 : %6.2f\n", avg);
-		System.out.println("─────────────────────────────");
-
+		for (int i = 0; i < 3; i++) {
+			System.out.println("학생 " + (i + 1) + "의 성적 ");
+			System.out.printf("국어 : %3d , ", student[0][i]);
+			System.out.printf("영어 : %3d , ", student[1][i]);
+			System.out.printf("수학 : %3d\n", student[2][i]);
+			System.out.println("────────────────────────────────────────────");
+		}
+		System.out.println("────────────────────────────────────────────");
+		System.out.printf("국어총점 : %3d  │  ", total[0]);
+		System.out.printf("국어평균 : %6.2f\n", avg[0]);
+		System.out.printf("영어총점 : %3d  │  ", total[1]);
+		System.out.printf("영어평균 : %6.2f\n", avg[1]);
+		System.out.printf("수학총점 : %3d  │  ", total[2]);
+		System.out.printf("수학평균 : %6.2f\n", avg[2]);
+		System.out.println("────────────────────────────────────────────");
 	}
 
 }
 
 class Grade {
-	static void inputGrade(int[] kors) {
+	static void inputGrade(int[][] student) {
 
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
@@ -79,16 +93,24 @@ class Grade {
 		System.out.println("│           성적  입력      │");
 		System.out.println("└───────────────────────────┘");
 		System.out.println();
+		for (int i = 0; i < 3; i++) {
+			System.out.printf("국어%d : ", i + 1);
+			student[0][i] = scan.nextInt();
+			System.out.printf("영어%d : ", i + 1);
+			student[1][i] = scan.nextInt();
+			System.out.printf("수학%d : ", i + 1);
+			student[2][i] = scan.nextInt();
 
-		for (int i = 0; i < 3; i++)
-			do {
-				System.out.printf("국어%d : ", i + 1);
-				kors[i] = scan.nextInt();
+			if (student[0][i] < 0 || 100 < student[0][i])
+				System.out.println("국어성적은 0~100까지의 범위만 입력이 가능합니다.");
 
-				if (kors[i] < 0 || 100 < kors[i])
-					System.out.println("국어성적은 0~100까지의 범위만 입력이 가능합니다.");
+			if (student[1][i] < 0 || 100 < student[1][i])
+				System.out.println("영어성적은 0~100까지의 범위만 입력이 가능합니다.");
 
-			} while (kors[i] < 0 || 100 < kors[i]);
+			if (student[2][i] < 0 || 100 < student[2][i])
+				System.out.println("수학성적은 0~100까지의 범위만 입력이 가능합니다.");
+
+		}
 
 		System.out.println("─────────────────────────────");
 
